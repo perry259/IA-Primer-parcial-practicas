aacda167ff75391a814312e196ff12e623fa52cc
perry259 <a23310375@ceti.mx>
2026-07-01 15:32:43 -0600

    Add example of string literals in Python

---
+++ Python literals - string.py
@@ -0,0 +1,5 @@
+# LAB#4 Python literals - string
+
+# El uso de \" permite incluir comillas dentro de un string delimitado por comillas
+# El \n separa el mensaje en tres renglones distintos
+print("\"I'm\"\n\"\"learning\"\"\n\"\"\"Python\"\"\"")
