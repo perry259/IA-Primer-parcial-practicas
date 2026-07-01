9f078903fb24a493d77e7cc3af4514307e43e1a1
perry259 <a23310375@ceti.mx>
2026-07-01 15:34:13 -0600

    Add simple input program for arithmetic operations
    Create a simple input program to perform basic arithmetic operations.

---
+++ Simple Input.py
@@ -0,0 +1,14 @@
+# LAB#9 Simple Input
+
+# Solicitamos datos al usuario y los convertimos a punto flotante (decimal)
+a = float(input("ingresa el primer numero: "))
+b = float(input("ingresa el segundo numero: "))
+
+# Mostramos los resultados de las 4 operaciones básicas
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+
+# El \n crea un espacio extra antes del mensaje final
+print("\nThat's all, folks!")
