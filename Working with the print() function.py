b7f900226cd75453ec1a49577ae406b0090f6d9d
perry259 <a23310375@ceti.mx>
2026-07-01 15:31:36 -0600

    Add examples of using the print() function

---
+++ Working with the print() function.py
@@ -0,0 +1,14 @@
+# LAB#1 Working with the print() function
+
+# Imprime una cadena de texto (string) con comillas dobles
+print("marco") 
+
+# Imprime una cadena de texto con comillas simples (totalmente válido)
+print('marco') 
+
+# Para imprimir 'marco' como variable, primero hay que definirla:
+nombre = "marco"
+print(nombre) 
+
+# Si quieres imprimir dos cosas en la misma línea, usa una coma:
+print("marco", "Python")
