d91e66db1ae4b9cd4108d6a01a02fbdda09a2736
perry259 <a23310375@ceti.mx>
2026-07-01 15:35:04 -0600

    Add script to evaluate if number is 100 or more

---
+++ Questions and answers.py
@@ -0,0 +1,9 @@
+# LAB#12 Questions and answers
+
+# Solicitamos un número entero al usuario
+n = int(input("Ingrese un numero: "))
+
+# Evaluamos la condición lógica:
+# Si n es 100 o más, imprimirá True.
+# Si n es menor a 100, imprimirá False.
+print(n >= 100)
