4b94ca636505294f6aa31b481ea3fb4d554c40ae
perry259 <a23310375@ceti.mx>
2026-07-01 15:33:39 -0600

    Add polynomial calculation in Operators and expressions.py

---
+++ Operators and expressions.py
@@ -0,0 +1,9 @@
+# LAB#7 Operators and expressions
+
+x = 0
+x = float(x) # Convertimos a flotante para mayor precisión
+
+# Calculamos el polinomio: 3x^3 - 2x^2 + 3x - 1
+y = (3 * x**3) - (2 * x **2) + (3 * x) - 1
+
+print("y =", y)
