156a18b6b7a9ff3be90b262074b5bea7ed9852ba
perry259 <a23310375@ceti.mx>
2026-07-01 15:31:58 -0600

    Create script for print() function demonstration
    Add a script demonstrating the print() function with custom separators and end characters.

---
+++ The print() function and its arguments.py
@@ -0,0 +1,7 @@
+# LAB#2 The print() function and its arguments
+
+# Imprime las tres palabras separadas por *** y termina con ... sin saltar de línea
+print("Programmin", "Essentials", "in", end="...", sep="***")
+
+# Esta palabra aparecerá justo después de los puntos suspensivos anteriores
+print("Python")
