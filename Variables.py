40b35008cc38be92d4ef9aa0874c5f46f327c607
perry259 <a23310375@ceti.mx>
2026-07-01 15:33:03 -0600

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,18 @@
+# LAB#5 Variables
+
+# Definimos los valores iniciales una sola vez
+john = 3
+mary = 5
+adam = 6
+
+# Operación de suma
+totalApples = john + mary + adam
+print("Total de manzanas (Suma):", totalApples)
+
+# Operación de multiplicación (reutilizando las variables)
+totalApples = john * mary * adam
+print("Resultado de la multiplicación:", totalApples)
+
+# Operación de resta
+totalApples = john - mary - adam
+print("Resultado de la resta:", totalApples)
